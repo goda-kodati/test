1b01a497a59df1d2f0a4ec65a2a0e4644be9d7c4
goda-kodati <79306718+goda-kodati@users.noreply.github.com>
2024-11-10 00:31:50 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test
+# GetAvatar
+# GetAvatar
---
+++ test.py
@@ -0,0 +1,5 @@
+import encodings
+print(encodings.__file__)
+
+
+/Applications/Blender.app/Contents/MacOS/Blender --background /path/to/your/test.blend --python /Users/godakodati/Desktop/CCN Project/GETAvatar-main/preprocess/render_aligned_thuman.py -- --device_id 0 --tot 1 --id 0
